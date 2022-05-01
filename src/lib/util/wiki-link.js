ad9395e0533899d051d54e3ac9031c5d20cf5f2f
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-05-01 14:47:14 +0000

    feat: init project

---
+++ src/lib/util/wiki-link.js
@@ -0,0 +1,64 @@
+import fs from 'fs';
+
+/**
+ *
+ * @param {string} dir
+ * @returns
+ */
+export function getMarkdownFiles(dir) {
+	/** @type {any[]} */
+	var results = [];
+	const items = fs.readdirSync(dir);
+	items.forEach((note) => {
+		note = `${dir}/${note}`;
+		const stat = fs.statSync(note);
+		if (stat && stat.isDirectory()) {
+			results = results.concat(getMarkdownFiles(note));
+		} else {
+			results.push(note);
+		}
+	});
+	return results.filter((f) => f.endsWith('.md'));
+}
+
+/**
+ *
+ * @param {string} path
+ */
+export function parseFileNameFromPath(path) {
+	if (path.includes('/')) {
+		const parsedFileFromPath = path.split('/')[path.split('/').length - 1];
+		return parsedFileFromPath.replace('.md', '');
+	} else {
+		return null;
+	}
+}
+
+/**
+ *
+ * @param {string} fileName
+ * @returns
+ */
+export function normalizeFileName(fileName) {
+	fileName = fileName
+		.replace('.md', '')
+		.replace('(', '')
+		.replace(')', '')
+		.split(' ')
+		.join('-')
+		.toLowerCase();
+
+	return fileName;
+}
+
+/**
+ *
+ * @param {string} fileName
+ * @param {string} dir
+ */
+export function toSlug(fileName, dir) {
+	if (fileName.includes(dir)) {
+		return (fileName = fileName.replace(dir, '').replace('.md', ''));
+	}
+	return '/';
+}
