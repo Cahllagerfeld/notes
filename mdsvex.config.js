ac21035060e8ade245fa24f48923fb25207be972
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-05-01 17:50:43 +0200

    feat: init project (#2)

---
+++ mdsvex.config.js
@@ -0,0 +1,41 @@
+import { defineMDSveXConfig as defineConfig } from 'mdsvex';
+import wikilink from 'remark-wiki-link';
+import path from 'path';
+import {
+	getMarkdownFiles,
+	parseFileNameFromPath,
+	normalizeFileName,
+	toSlug
+} from './src/lib/util/wiki-link.js';
+
+const config = defineConfig({
+	extensions: ['.svelte.md', '.md', '.svx'],
+
+	smartypants: {
+		dashes: 'oldschool'
+	},
+
+	remarkPlugins: [
+		[
+			wikilink,
+			{
+				pageResolver: (permalink) => {
+					const dir = path.join(process.cwd(), 'src/routes');
+					const allFiles = getMarkdownFiles(dir);
+					const result = allFiles.find((file) => {
+						const parsedFileName = parseFileNameFromPath(file);
+						const match = normalizeFileName(permalink) === normalizeFileName(parsedFileName);
+						return match;
+					});
+					return result !== undefined && result.length > 0 ? [toSlug(result, dir)] : '/';
+				},
+				hrefTemplate: (permalink) => {
+					return `${permalink}`;
+				}
+			}
+		]
+	],
+	rehypePlugins: []
+});
+
+export default config;
---
+++ svelte.config.js
@@ -0,0 +1,37 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			postcss: true
+		}),
+		mdsvex(mdsvexConfig)
+	],
+
+	kit: {
+		files: {
+			lib: 'src/lib'
+		},
+		adapter: adapter(),
+		vite: {
+			server: {
+				hmr: {
+					clientPort: process.env.GITPOD_WORKSPACE_URL ? 443 : 3000,
+					host: process.env.GITPOD_WORKSPACE_URL
+						? process.env.GITPOD_WORKSPACE_URL.replace('https://', '3000-')
+						: 'localhost'
+				}
+			}
+		}
+	}
+};
+
+export default config;
