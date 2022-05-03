fb0a276fdcdc1a018f81ba2d82fbe1b267b71c15
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-05-03 19:37:14 +0000

    feat: init slug route

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,16 @@
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch(`/api/notes.json`);
+		const posts = await res.json();
+		console.log(posts);
+		return {
+			stuff: {
+				posts
+			}
+		};
+	}
+</script>
+
 <script>
 	import '../app.css';
 </script>
