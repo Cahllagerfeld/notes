b0d6008fa2cfde269a3e0f588e2f073c1db6b589
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-05-04 14:02:05 +0000

    feat: move notes to routes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,16 +1,3 @@
-<script context="module">
-	export async function load({ fetch }) {
-		const res = await fetch(`/api/notes.json`);
-		const posts = await res.json();
-		console.log(posts);
-		return {
-			stuff: {
-				posts
-			}
-		};
-	}
-</script>
-
 <script>
 	import '../app.css';
 </script>
