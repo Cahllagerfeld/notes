63ad119a8d01da12e7ba4fdb1e9d159f65f46fa2
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-05-08 17:42:40 +0000

    style: adjust header

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
+	import { navbarItems } from '$lib/contents/layout/header-items';
+	import { page } from '$app/stores';
 </script>
 
-<header>
-	<div class="mt-16 text-4xl dark:text-[#FFFFFFDE]">Julian's Digital Garden 🌴</div>
+<header class="flex h-16 items-center justify-between">
+	<div class="text-4xl dark:text-[#FFFFFFDE]"><a href="/">Julian's Page 🌴</a></div>
+	<ul class="space-x-4">
+		{#each navbarItems as item}
+			<li class:active={$page.url.pathname.includes(item.href)} class="inline">
+				<a href={item.href}>{@html item.title}</a>
+			</li>
+		{/each}
+	</ul>
 </header>
+
+<style lang="postcss">
+	.active {
+		@apply font-semibold text-darkred;
+	}
+</style>
---
+++ src/lib/contents/layout/header-items.ts
@@ -0,0 +1,6 @@
+export const navbarItems: { title: string; href: string }[] = [
+	{
+		title: 'Digital Garden',
+		href: '/notes/000-index'
+	}
+];
