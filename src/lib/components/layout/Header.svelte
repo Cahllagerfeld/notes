1a1609594f8f1cd1e7106b0e8247cb5880dcb726
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-05-08 09:14:22 +0000

    style: init header

---
+++ src/lib/components/layout/Header.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<header>
+	<div class="mt-16 text-4xl dark:text-[#FFFFFFDE]">Julian's Digital Garden 🌴</div>
+</header>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,9 +11,11 @@
 </script>
 
 <script lang="ts">
+	import Header from '../lib/components/layout/Header.svelte';
 	import '../app.css';
 </script>
 
 <div class="mx-auto md:container">
+	<Header />
 	<slot />
 </div>
