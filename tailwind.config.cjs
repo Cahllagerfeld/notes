3302c4795ddba0ca521beae69b1b813912549449
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-05-08 10:19:06 +0200

    style: add color palette to tailwind-config (#15)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				black: '#181717',
+				offwhite: '#d4d4d4',
+				darkred: '#8d0000',
+				lightgray: '#656464',
+				darkgray: '#2b2a2a'
+			}
+		}
 	},
 
 	plugins: [require('@tailwindcss/typography')]
