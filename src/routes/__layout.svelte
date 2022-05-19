7c78d5195a0d8313c1b9a6f968d7861475d32ab3
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-05-19 07:21:00 +0000

    fix: breakpoints & spacing

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex min-h-screen flex-col md:mx-0">
 	<Header />
-	<main class="mx-auto w-full px-0 lg:container">
+	<main class="mx-auto w-full px-4 md:container">
 		<slot />
 	</main>
 </div>
