7c9f36f98350fd561c2807ffa081dd40200d0e0b
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-05-23 17:01:29 +0000

    feat: init search-endpoint

---
+++ src/routes/api/search.ts
@@ -0,0 +1,38 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import path from 'path';
+import { getMarkdownFiles } from '$lib/util/markdown';
+import { compile } from 'mdsvex';
+import fs from 'fs';
+import mdsvexConfig from '../../../mdsvex.config.js';
+import Fuse from 'fuse.js';
+
+export const get: RequestHandler = async ({ url }) => {
+	const param = url.searchParams.get('q') || '';
+	const dir = 'src/routes';
+	const items = getMarkdownFiles(path.join(dir));
+	const parsedItems = await Promise.all(
+		items.map(async (item) => {
+			const content = fs.readFileSync(item, 'utf8');
+			const compiled = await compile(content, mdsvexConfig);
+			compiled!.code = compiled!.code
+				.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')
+				.replace(/<\/?[^>]+(>|$)/g, '');
+			return compiled;
+		})
+	);
+	const fuse = new Fuse(parsedItems, {
+		includeScore: true,
+		keys: [
+			{ name: 'code', weight: 0.2 },
+			{ name: 'data.fm.title', weight: 0.5 },
+			{ name: 'data.fm.tags', weight: 0.3 }
+		]
+	});
+
+	return {
+		status: 200,
+		body: {
+			result: fuse.search(param)
+		}
+	};
+};
