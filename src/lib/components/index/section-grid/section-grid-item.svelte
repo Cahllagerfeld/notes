2bc6ce5fac3342bb4877fd182fb768ccddabab1c
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-06-19 09:27:17 +0000

    feat: init index

---
+++ src/lib/components/index/section-grid/section-grid-item.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { SectionItemConfig } from '$lib/types';
+
+	export let itemConfig: SectionItemConfig;
+</script>
+
+<div class="w-full max-w-md rounded-2xl p-4 shadow-normal">
+	<a sveltekit:prefetch class="flex flex-col items-center justify-center" href={itemConfig.href}>
+		<div class="mb-2">{@html itemConfig.title}</div>
+		<img width="100" height="100" src={itemConfig.icon} alt={itemConfig.alt} />
+	</a>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,10 @@ const config = {
 				darkred: '#8d0000',
 				lightgray: '#bfbfbf',
 				darkgray: '#2b2a2a'
+			},
+			boxShadow: {
+				normal: '0 0.625rem 1.5rem 0rem rgba(0, 0, 0, 0.08)',
+				medium: '0 1.25rem 2.5rem rgba(0, 0, 0, 0.1)'
 			}
 		}
 	},
