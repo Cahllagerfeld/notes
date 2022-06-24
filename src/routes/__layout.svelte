27a062198b2298f3e6399d02511c6499932c9554
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-06-24 20:39:15 +0000

    style: add padding to main

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -23,9 +23,9 @@
 	setContext(key, markdown);
 </script>
 
-<div class="flex min-h-screen flex-col md:mx-0">
+<div class="flex min-h-screen  flex-col md:mx-0">
 	<Header />
-	<main class="mx-auto w-full px-4 md:container md:px-0">
+	<main class="mx-auto w-full px-4 pb-12 md:container md:px-0">
 		<slot />
 	</main>
 	<Footer />
