cb3c13cfbc966607d621c39b67e66ff3ae530dc5
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-06-25 14:49:00 +0000

    test: update tests

--- tests/footer.spec.ts
+++ tests/footer.spec.ts
@@ -3,13 +3,13 @@ import { expect, test } from '@playwright/test';
 test('twitter footer link', async ({ page }) => {
 	await page.goto('/');
 
-	await Promise.all([page.waitForNavigation(), page.locator('i').first().click()]);
+	await Promise.all([page.waitForNavigation(), page.locator('i').nth(1).click()]);
 	expect(page.url()).toContain('twitter.com/CahlLagerfeld');
 });
 
 test('github footer link', async ({ page }) => {
 	await page.goto('/');
 
-	await Promise.all([page.waitForNavigation(), page.locator('i').nth(1).click()]);
+	await Promise.all([page.waitForNavigation(), page.locator('i').nth(2).click()]);
 	expect(page.url()).toContain('github.com/Cahllagerfeld');
 });
