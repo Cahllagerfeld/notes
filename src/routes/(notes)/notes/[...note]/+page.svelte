a84bd8dc31b01392d944deefa87a83ec820b32f5
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-12-18 10:15:59 +0000

    fix: favicon

--- src/routes/(notes)/notes/[...note]/+page.svelte
+++ src/routes/(notes)/notes/[...note]/+page.svelte
@@ -5,6 +5,9 @@
 	export let data: PageServerData;
 </script>
 
+<svelte:head>
+	<title>Digital Garden | {data.meta.title}</title>
+</svelte:head>
 <NotesLayout created={data.meta.created} tags={data.meta.tags} title={data.meta.title}>
 	{@html data.html}
 </NotesLayout>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
 	import SectionGrid from '$lib/components/index/section-grid/section-grid.svelte';
 </script>
 
+<svelte:head>
+	<title>Julian's Page</title>
+</svelte:head>
 <div class="flex flex-col items-center">
 	<SectionGrid />
 </div>
