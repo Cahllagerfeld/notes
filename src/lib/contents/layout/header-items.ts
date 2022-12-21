bedb7152391c803c0762685020685b9e1d361cfc
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-12-21 17:28:22 +0000

    fix: navbar (#94)

--- src/lib/contents/layout/header-items.ts
+++ src/lib/contents/layout/header-items.ts
@@ -1,5 +1,10 @@
 export const navbarItems: { title: string; href: string; iconClasses: string }[] = [
 	{
+		title: 'Tags',
+		href: '/tags',
+		iconClasses: 'fa-solid fa-hashtag'
+	},
+	{
 		iconClasses: 'fa-solid fa-seedling',
 		title: 'Digital Garden',
 		href: '/notes/index'
--- tests/navbar.spec.ts
+++ tests/navbar.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test('left-side navitem to route back to index', async ({ page }) => {
-	await page.goto('/notes');
+	await page.goto('/notes/index');
 
 	await Promise.all([page.waitForNavigation(), page.locator("text=Julian's Page 🌴").click()]);
 	expect(page.url()).toBe('http://localhost:4173/');
