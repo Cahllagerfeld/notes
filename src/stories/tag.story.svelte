4f4a80fd7f9756ab82f4f92befd27ab63df6b5ce
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-12-22 08:39:48 +0000

    feat: add control

--- src/stories/tag.story.svelte
+++ src/stories/tag.story.svelte
@@ -2,8 +2,13 @@
 	import type { Hst as HstType } from '@histoire/plugin-svelte';
 	export let Hst: HstType;
 	import Tag from '$lib/components/notes/tag.svelte';
+
+	let label = 'Tag';
 </script>
 
 <Hst.Story title="Tag">
-	<Tag label="Test" />
+	<Tag {label} />
+	<svelte:fragment slot="controls">
+		<Hst.Text bind:value={label} title="Tag label" />
+	</svelte:fragment>
 </Hst.Story>
