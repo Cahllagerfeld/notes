0d3d16d3d4d691ecfe045f6c85392f76249cc941
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-12-22 08:27:21 +0000

    feat: add histoire

---
+++ src/stories/tag.story.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { Hst as HstType } from '@histoire/plugin-svelte';
+	export let Hst: HstType;
+	import Tag from '$lib/components/notes/tag.svelte';
+</script>
+
+<Hst.Story title="Tag">
+	<Tag label="Test" />
+</Hst.Story>
