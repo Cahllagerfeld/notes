617327857bbfb0655e7f508cc7fc74fe69624b3e
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-12-24 13:41:34 +0000

    feat: glowing button

---
+++ src/lib/components/buttons/glowing-button.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let type: 'button' | 'anchor' = 'button';
+
+	const classMap = {
+		button: '',
+		anchor: 'block'
+	};
+</script>
+
+<div class="group relative inline-block">
+	<div
+		class="absolute -inset-0.5 rounded-lg bg-skin-accent opacity-75 blur transition-all duration-200 group-hover:opacity-100"
+	/>
+	<svelte:element
+		this={type == 'anchor' ? 'a' : 'button'}
+		class="relative rounded-lg bg-skin-background {classMap[type]}"
+		{...$$restProps}
+	>
+		<slot />
+	</svelte:element>
+</div>
---
+++ src/stories/buttons/glowing-button.story.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Hst as HstType } from '@histoire/plugin-svelte';
+	export let Hst: HstType;
+	import GlowingButton from '$lib/components/buttons/glowing-button.svelte';
+</script>
+
+<div class="m-8">
+	<Hst.Story title="Glowing Button">
+		<GlowingButton>
+			<div class="py-2 px-4">Glowing Button</div>
+		</GlowingButton>
+	</Hst.Story>
+</div>
